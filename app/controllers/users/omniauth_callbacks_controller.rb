2147623aab77a4c0a82962dd5b395ce88a1e4d65
Santiago Giraldo M <santi_rm8@hotmail.com>
2017-11-09 23:21:15 -0500

    fix error in omniaunt facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,13 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      if (@user.missing_data?)
-        sign_in @user, event: :authentication
-        flash[:alert] = t("fill_all_fields")
-        redirect_to edit_user_registration_url
-      else
-        sign_in_and_redirect @user
-      end
+      sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
