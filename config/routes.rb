1a7e75eec2a5843cfe0b84958c1c5f4ec8f37961
Santiago Giraldo M <santi_rm8@hotmail.com>
2017-11-13 15:34:05 -0500

    Add route admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks",
                                     registrations: "users/registrations" }
 
+
+  namespace :admin do
+    root to: "dashboards#index"
+
+    resources :users,    only: [:index, :destroy] do
+      collection do
+        match 'search' => 'users#search', via: [:get, :post], as: :search
+      end
+    end
+    
+  end
+
   root 'pages#home'
 
 end
