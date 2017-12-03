d44e95eeb55e2dce9ffef8edabc1c8f23a4e1e68
Santiago Giraldo M <santi_rm8@hotmail.com>
2017-12-03 10:39:15 -0500

    Add config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://saloon-backend.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
     address:         'smtp.sendgrid.net',
