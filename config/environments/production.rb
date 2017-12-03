c7db75892e9785822153cc4bc11088696bb8c158
Santiago Giraldo M <santi_rm8@hotmail.com>
2017-12-03 11:43:00 -0500

    Fix error config action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,24 +88,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
-  # ActionMailer::Base.smtp_settings = {
-  #   address:         'smtp.sendgrid.net',
-  #   port:            '587',
-  #   authentication:  :plain,
-  #   user_name:       ENV['SENDGRID_USERNAME'],
-  #   password:        ENV['SENDGRID_PASSWORD'],
-  #   domain:          'heroku.com',
-  #   enable_starttls_auto:  true
-  # }
-
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         'heroku.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
+  ActionMailer::Base.smtp_settings = {
+    address:         'smtp.sendgrid.net',
+    port:            '587',
+    authentication:  :plain,
+    user_name:       ENV['SENDGRID_USERNAME'],
+    password:        ENV['SENDGRID_PASSWORD'],
+    domain:          'heroku.com',
+    enable_starttls_auto:  true
   }
 
   # Amazon S3
